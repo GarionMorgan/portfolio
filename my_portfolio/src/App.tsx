4f28321d108df7e4503c7898fa2777aba07e6bf1
github.com/GarionMorgan <garmorgan14@yahoo.com>
2026-03-13 14:53:49 -0500

    Centered application

--- my_portfolio/src/App.tsx
+++ my_portfolio/src/App.tsx
@@ -7,10 +7,10 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="app__container">
       <Header />
       <Footer />
-    </>
+    </div>
   );
 }
 
