ee4b308e6b30944479375bfd8edb161e8f25c85a
github.com/GarionMorgan <garmorgan14@yahoo.com>
2026-03-14 22:05:57 -0500

    Added more to Header.tsx file

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <div className="header__container">
+      <p>My Portfolio</p>
+      <p>Home</p>
+      <p>Projects</p>
+      <p>About</p>
+      <p>Contact</p>
+    </div>
+  );
+};
