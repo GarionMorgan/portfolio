7c3622cd4bb4e6bb921274795c1107d45a2fce50
github.com/GarionMorgan <garmorgan14@yahoo.com>
2026-03-18 21:02:38 -0500

    Added information to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,14 @@ export const Header = () => {
   console.log('test');
   return (
     <div className="header__container">
-      <h2>Header</h2>
+      <h2>Garion Morgan</h2>
+      <h2>Software Developer</h2>
+      <h2>
+        Building reliable systems and empowering teams through clarity,
+        precision, and purpose.
+      </h2>
+      <button>View Projects</button>
+      <button>Download Resume</button>
       <NavBar />
     </div>
   );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,6 @@ export const NavBar = () => {
   return (
     <div className="header__container">
       <Link to="/">Home</Link>
-      <Link to="/projects">Projects</Link>
       <Link to="/about">About</Link>
       <Link to="/contact">Contact</Link>
     </div>
